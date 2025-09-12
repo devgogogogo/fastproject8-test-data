373f930b57bbcd4b637208e93fcea0b9883d914c
kwihyeon <imleo322@gmail.com>
2025-09-12 16:23:19 +0900

    feat : #6 테이블 스키마, 스키마 필드 도메인 설계
    테이블 스키마와 스키마 필드를 다룰때 필요한 데이터를 클래스로 설계함.

---
+++ src/main/java/com/fastcampus/testdata/domain/SchemaField.java
@@ -0,0 +1,17 @@
+package com.fastcampus.testdata.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+public class SchemaField {
+
+    private String fieldName;
+    private String mockDataType;
+    private Integer fieldOrder;
+    private Integer blankPercent;
+    private String typeOptionJson;
+    private String forceValue;
+
+}
